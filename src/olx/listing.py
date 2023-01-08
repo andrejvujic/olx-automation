46b488694d70c658474b516bbd113a800bddea8a
Andrej Vujić <vujicandrej365@gmail.com>
2023-01-09 00:34:56 +0100

    create method for deleting listing

--- src/olx/listing.py
+++ src/olx/listing.py
@@ -1,10 +1,15 @@
 from bs4.element import Tag
 
 from .session import Session
+from .routes import DELETE_LISTING_ROUTE_PATH
 
 
 class Listing:
-    def __init__(self, listing_element: Tag, session: Session) -> None:
+    def __init__(
+        self,
+        listing_element: Tag,
+        session: Session,
+    ) -> None:
         self.ID = listing_element.get(
             "id",
         ).replace(
@@ -24,6 +29,19 @@ class Listing:
                 "class": "na",
             }
         ).get_text()
+
+        self.element = listing_element
+        self.session = session
+
+    def delete(self) -> None:
+        PAYLOAD = {
+            "id": self.ID,
+        }
+
+        self.session.instance.post(
+            DELETE_LISTING_ROUTE_PATH,
+            data=PAYLOAD,
+        )
 
     def __repr__(self) -> str:
         return f"""
