026953852e28440bd7a324b1f25fdd370ee8a2b0
Andrej Vujić <vujicandrej365@gmail.com>
2023-01-08 19:18:25 +0100

    login with credentials

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from credentials import Credentials
+from olx.user import User
 
 CREDENTIALS_FILE_PATH = "./credentials.json"
 
 credentials = Credentials(
     CREDENTIALS_FILE_PATH,
 )
+
+user = User(
+    credentials.username,
+    credentials.password,
+)
+
+user.login()
