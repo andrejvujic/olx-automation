b6d84a0faf64e327bcfdd56bfde8c5861106a3ec
Andrej Vujić <vujicandrej365@gmail.com>
2023-01-08 19:08:55 +0100

    attach headers to instance of requests.session

--- src/olx/session.py
+++ src/olx/session.py
@@ -7,16 +7,15 @@ from .routes import INDEX_ROUTE_PATH
 class Session:
     def __init__(self) -> None:
         self.session = requests.session()
-        self.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.3"
+
+        HEADERS = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.3",
+        }
+        self.session.headers = HEADERS
 
     def get_CSRF_token(self) -> None:
-        HEADERS = {
-            "User-Agent": self.user_agent,
-        }
-
         r = self.session.get(
             INDEX_ROUTE_PATH,
-            headers=HEADERS,
         )
 
         soup = BeautifulSoup(
